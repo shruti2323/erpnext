e8867d56312c896d7119c183a36137c1f133ae05
Alchez <Alchez@users.noreply.github.com>
2018-05-02 18:50:58 +0530

    [WIP] Add contracts in the website portal (#26)
    * Add contracts in the website portal
    
    * Display basic details for individual contracts
    
    * clicking on I Agree button will update the Contract doctype
    
    * fine tuining of CSS
    
    * Add fulfilment status and details

--- erpnext/crm/doctype/contract/contract.py
+++ erpnext/crm/doctype/contract/contract.py
@@ -5,41 +5,130 @@
 from __future__ import unicode_literals
 
 import frappe
+from awesome_cart.compat.customer import get_current_customer
+from frappe import _
 from frappe.model.document import Document
-from frappe.utils import getdate, nowdate
+from frappe.utils import getdate, nowdate, now_datetime
 
 
 class Contract(Document):
 	def validate(self):
 		self.validate_dates()
-		self.update_status()
+		self.update_contract_status()
+		self.update_fulfilment_status()
 
 	def on_update_after_submit(self):
-		self.update_status()
+		self.update_contract_status()
 
 	def validate_dates(self):
-		if self.end_date < self.start_date:
+		if self.end_date and self.end_date < self.start_date:
 			frappe.throw("End Date cannot be before Start Date!")
 
-
-	def update_status(self):
+	def update_contract_status(self):
 		if self.is_signed:
 			self.status = get_status(self.start_date, self.end_date)
 		else:
 			self.status = "Unsigned"
 
+	def update_fulfilment_status(self):
+		self.fulfilment_status = ""
+
+		if self.requires_fulfilment:
+			fulfilled_terms = self.get_fulfilled_terms()
+
+			if not fulfilled_terms:
+				self.fulfilment_status = "Unfulfilled"
+			elif fulfilled_terms < len(self.fulfilment_terms):
+				self.fulfilment_status = "Partially Unfulfilled"
+			elif fulfilled_terms == len(self.fulfilment_terms):
+				self.fulfilment_status = "Fulfilled"
+
+			if self.fulfilment_deadline:
+				now_date = getdate(nowdate())
+				deadline_date = getdate(self.fulfilment_deadline)
+
+				if now_date > deadline_date:
+					self.fulfilment_status = "Lapsed"
+
+	def get_fulfilled_terms(self):
+		return len([term for term in self.fulfilment_terms if term.fulfilled])
+
+	def has_website_permission(self, doc, ptype, user, verbose=False):
+		"""
+		Returns `True` if the contract party name matches
+		with the logged in user/customer
+		"""
+
+		customer = get_current_customer()
+
+		if customer:
+			return doc.party_name == customer.name
+
 
 def get_status(start_date, end_date):
 	if not end_date:
 		return "Active"
+
+	start_date = getdate(start_date)
+	end_date = getdate(end_date)
 	now_date = getdate(nowdate())
-	status = "Active" if start_date < now_date < end_date else "Inactive"
-	return status
+
+	return "Active" if start_date < now_date < end_date else "Inactive"
 
 
 def update_status_for_contracts():
 	contracts = frappe.get_all("Contract", filters={"is_signed": True, "docstatus": 1}, fields=["name", "start_date", "end_date"])
+
 	for contract in contracts:
 		status = get_status(contract.get("start_date"),
 							contract.get("end_date"))
+
 		frappe.db.set_value("Contract", contract.get("name"), "status", status)
+
+
+def get_list_context(context=None):
+	from erpnext.controllers.website_list_for_contact import get_list_context
+
+	list_context = get_list_context(context)
+	list_context.update({
+		'show_sidebar': True,
+		'show_search': True,
+		'no_breadcrumbs': True,
+		"row_template": "templates/includes/contract_row.html",
+		'get_list': get_contract_list,
+		'title': _("Contracts")
+	})
+
+	return list_context
+
+
+def get_contract_list(doctype, txt, filters, limit_start, limit_page_length=20, order_by=None):
+	from frappe.www.list import get_list
+
+	user = frappe.session.user
+	ignore_permissions = False
+
+	if not filters:
+		filters = []
+
+	if user != "Guest":
+		customer = get_current_customer()
+
+		if customer:
+			filters.append(("Contract", "party_name", "=", customer.name))
+			ignore_permissions = True
+
+	return get_list(doctype, txt, filters, limit_start, limit_page_length, ignore_permissions=ignore_permissions)
+
+
+@frappe.whitelist()
+def accept_contract_terms(dn, signee):
+	contract = frappe.get_doc("Contract", dn)
+
+	contract.is_signed = True
+	contract.signee = signee
+	contract.signed_on = now_datetime()
+	contract.flags.ignore_permissions = True
+
+	contract.save()
+	frappe.db.commit()
--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -109,6 +109,13 @@ website_route_rules = [
 			"parents": [{"label": _("Shipments"), "route": "shipments"}]
 		}
 	},
+	{"from_route": "/contracts", "to_route": "Contract"},
+	{"from_route": "/contracts/<path:name>", "to_route": "contract_detail",
+		"defaults": {
+			"doctype": "Contract",
+			"parents": [{"label": _("Contracts"), "route": "contracts"}]
+		}
+	},
 	{"from_route": "/rfq", "to_route": "Request for Quotation"},
 	{"from_route": "/rfq/<path:name>", "to_route": "rfq",
 		"defaults": {
@@ -169,7 +176,8 @@ has_website_permission = {
 	"Timesheet": "erpnext.controllers.website_list_for_contact.has_website_permission",
 	"Lab Test": "erpnext.healthcare.web_form.lab_test.lab_test.has_website_permission",
 	"Consultation": "erpnext.healthcare.web_form.prescription.prescription.has_website_permission",
-	"Patient Appointment": "erpnext.healthcare.web_form.patient_appointments.patient_appointments.has_website_permission"
+	"Patient Appointment": "erpnext.healthcare.web_form.patient_appointments.patient_appointments.has_website_permission",
+	"Contract": "erpnext.crm.doctype.contract.contract.has_website_permission"
 }
 
 dump_report_map = "erpnext.startup.report_data_map.data_map"
