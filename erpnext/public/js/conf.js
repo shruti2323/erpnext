59dcd38ce1d39dfc244e480736c4a3c78a503db7
Valmik Jangla <mail@valmik.in>
2018-05-08 18:13:49 +0530

    Removed extra rows from the help menu

--- erpnext/public/js/conf.js
+++ erpnext/public/js/conf.js
@@ -23,14 +23,6 @@ $(document).bind('toolbar_setup', function() {
 
 	// additional help links for erpnext
 	var $help_menu = $('.dropdown-help ul .documentation-links');
-
-	$('<li><a data-link-type="forum" href="https://discuss.erpnext.com" \
-		target="_blank">'+__('User Forum')+'</a></li>').insertBefore($help_menu);
-	$('<li><a href="https://gitter.im/frappe/erpnext" \
-		target="_blank">'+__('Chat')+'</a></li>').insertBefore($help_menu);
-	$('<li><a href="https://github.com/frappe/erpnext/issues" \
-		target="_blank">'+__('Report an Issue')+'</a></li>').insertBefore($help_menu);
-
 });
 
 
