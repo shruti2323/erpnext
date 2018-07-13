91497c6340a1269bb6e64c942fc6c7209a09ea2d
Rohan Bansal <rohan@digithinkit.com>
2018-07-13 17:51:56 +0530

    Add dashboard to Warranty Claim

---
+++ erpnext/support/doctype/warranty_claim/warranty_claim_dashboard.py
@@ -0,0 +1,27 @@
+from frappe import _
+
+
+def get_data():
+	return {
+		'fieldname': 'warranty_claim',
+		'non_standard_fieldnames': {},
+		'internal_links': {},
+		'transactions': [
+			{
+				'label': _('Reference'),
+				'items': ['Quotation']
+			},
+			{
+				'label': _('Stock'),
+				'items': ['Stock Entry']
+			},
+			{
+				'label': _('Work'),
+				'items': ['Production Order']
+			},
+			{
+				'label': _('Fulfilment'),
+				'items': ['Sales Invoice', 'Delivery Note']
+			}
+		]
+	}
