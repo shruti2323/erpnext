5f75ff25c6a280c363e1b20f17eb94b6520a1313
Diksha <dikshajadhav11.dj@gmail.com>
2019-12-23 15:52:08 +0530

    fix: refactor code

--- erpnext/hr/doctype/attendance/attendance_list.js
+++ erpnext/hr/doctype/attendance/attendance_list.js
@@ -1,9 +1,9 @@
 frappe.listview_settings['Attendance'] = {
 	add_fields: ["status", "attendance_date"],
 	get_indicator: function (doc) {
-		if (doc.status == "Present" || "Work From Home") {
+		if (doc.status == "Present" || doc.status == "Work From Home") {
 			return [__(doc.status), "green", "status,=," + doc.status];
-		} else if (doc.status == "Absent" || "On Leave") {
+		} else if (doc.status == "Absent" || doc.status == "On Leave") {
 			return [__(doc.status), "red", "status,=," + doc.status];
 		} else if (doc.status == "Half Day") {
 			return [__(doc.status), "orange", "status,=," + doc.status];
