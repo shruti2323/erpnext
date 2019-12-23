6527eb8969de7bb954ce0ef1e33f3473cabcbb9d
Diksha <dikshajadhav11.dj@gmail.com>
2019-12-23 15:43:20 +0530

    feat(attendance): add work from home option in status

--- erpnext/hr/doctype/attendance/attendance_list.js
+++ erpnext/hr/doctype/attendance/attendance_list.js
@@ -1,6 +1,12 @@
 frappe.listview_settings['Attendance'] = {
 	add_fields: ["status", "attendance_date"],
-	get_indicator: function(doc) {
-		return [__(doc.status), doc.status=="Present" ? "green" : "darkgrey", "status,=," + doc.status];
+	get_indicator: function (doc) {
+		if (doc.status == "Present" || "Work From Home") {
+			return [__(doc.status), "green", "status,=," + doc.status];
+		} else if (doc.status == "Absent" || "On Leave") {
+			return [__(doc.status), "red", "status,=," + doc.status];
+		} else if (doc.status == "Half Day") {
+			return [__(doc.status), "orange", "status,=," + doc.status];
+		}
 	}
 };
