f7778fa72b810342742a07c7ef8afcf760dec5d3
Himanshu <himanshuwarekar@yahoo.com>
2021-07-01 15:02:42 +0530

    Update erpnext/stock/doctype/stock_entry/stock_entry.py

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -708,7 +708,7 @@ class StockEntry(StockController):
 				row_id = 'Row '+str(item_list[index].idx)
 				items_without_batch_no.append(row_id)
 			
-		if (items_without_batch_no):
+		if items_without_batch_no:
 			frappe.throw('Batch number is mandatory for Item at : <br><b>' + ', '.join(items_without_batch_no))
 
 	def update_batch_with_customer_provided_item(self):
