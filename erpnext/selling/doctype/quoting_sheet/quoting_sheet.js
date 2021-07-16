f8ba0555a8acd23fda41a710ee54e3886dc0cf71
sahilk25 <sahil.kamboj25@gmail.com>
2021-07-16 11:03:37 +0530

    feat: bulkdiscount (#1397)

--- erpnext/selling/doctype/quoting_sheet/quoting_sheet.js
+++ erpnext/selling/doctype/quoting_sheet/quoting_sheet.js
@@ -1,9 +1,72 @@
 // Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 frappe.ui.form.on('Quoting Sheet', {
-	item_code: function (frm) {
-		if (frm.doc.currency == frappe.sys_defaults.currency) {
-			frm.set_value("conversion_rate", 1.0);
+	onload: (frm) => {
+		frm.set_query("quote_to", function() {
+			return{
+				"filters": {
+					"name": ["in", ["Customer", "Lead"]],
+				}
+			};
+		});
+
+		frm.set_query("bom", () => {
+			if (frm.doc.item_code) {
+				return {
+					filters: {
+						"item": frm.doc.item_code
+					}
+				};
+			}
+		});
+	},
+
+	refresh:(frm) => {
+		frm.trigger('set_dynamic_field_label');
+	},
+
+	quote_to:(frm) => {
+		frm.trigger('set_dynamic_field_label');
+	},
+
+	set_dynamic_field_label: function(frm){
+		if (frm.doc.quote_to == "Customer")
+		{
+			frm.set_df_property("party_name", "label", "Customer");
+			frm.fields_dict.party_name.get_query = null;
+		}
+
+		if (frm.doc.quote_to == "Lead")
+		{
+			frm.set_df_property("party_name", "label", "Lead");
+
+			frm.fields_dict.party_name.get_query = function() {
+				return{	query: "erpnext.controllers.queries.lead_query" };
+			};
+		}
+	},
+
+	bom: function (frm) {
+		if (frm.doc.bom) {
+			frappe.call({
+				method: "get_raw_materials",
+				doc: frm.doc,
+				callback: () => {
+					frm.refresh_field("raw_material_items");
+					frm.refresh_field("rm_cost");
+				},
+			});
+		}
+	},
+	bulk_discount_scheme: function (frm) {
+		if (frm.doc.bulk_discount_scheme) {
+			frappe.call({
+				method: "get_bulk_discount",
+				doc: frm.doc,
+				callback: () => {
+					frm.refresh_field("bulk_discount");
+				},
+			});
 		}
 	},
 
@@ -17,13 +80,27 @@ frappe.ui.form.on('Quoting Sheet', {
 				frm.reload_doc();
 			},
 		});
-	}
+	},
+
+	calculate_total_raw_material_cost: function (frm) {
+		frappe.call({
+			method: "calculate_total_raw_material_cost",
+			doc: frm.doc,
+			callback: (res) => {
+				if (res.message) {
+					frm.set_value("rm_cost", res.message);
+					frm.refresh_field("rm_cost");
+				}
+
+			}
+		});
+	},
 });
 
 frappe.ui.form.on("Quoting Sheet Item", {
 	item_code: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
-		if (!row.item_code) {return;}
+		if (!row.item_code) { return; }
 		frappe.call({
 			method: "erpnext.selling.doctype.quoting_sheet.quoting_sheet.get_item_details_quoting_sheet",
 			args: {
@@ -42,6 +119,7 @@ frappe.ui.form.on("Quoting Sheet Item", {
 		if (row.qty && row.rate) {
 			let amount = row.qty * row.rate;
 			frappe.model.set_value(cdt, cdn, "amount", amount);
+			frm.trigger("calculate_total_raw_material_cost");
 		}
 	},
 
@@ -50,6 +128,7 @@ frappe.ui.form.on("Quoting Sheet Item", {
 		if (row.qty && row.rate) {
 			let amount = row.qty * row.rate;
 			frappe.model.set_value(cdt, cdn, "amount", amount);
+			frm.trigger("calculate_total_raw_material_cost");
 		}
 	},
 
