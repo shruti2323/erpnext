ca924cabda9ecb055c670f4a918e2a69f52a95a7
shruti2323 <shruti.kamble@extrapreneurs-india.com>
2021-08-25 15:15:45 +0200

    chore: fix the slider issue

--- erpnext/stock/doctype/stock_reconciliation/test_stock_reconciliation.py
+++ erpnext/stock/doctype/stock_reconciliation/test_stock_reconciliation.py
@@ -98,16 +98,15 @@ class TestStockReconciliation(unittest.TestCase):
 		items = get_items("_Test Warehouse Group 1 - _TC", nowdate(), nowtime(), "_Test Company")
 		
 		# Created the items list as the method get_items have inconsistent behavior
- 		# due to the two opposite values being passed which results in an empty items list
+		# due to the two opposite values being passed which results in an empty items list
 		if not items:
-			items = {	'item_code': '_Test Stock Reco Item',
-						'warehouse': '_Test Warehouse Ledger 1 - _TC',
-						'qty': 100.0
-      				}
+			items = {'item_code': '_Test Stock Reco Item',
+					'warehouse': '_Test Warehouse Ledger 1 - _TC',
+					'qty': 100.0}
 			self.assertEqual(["_Test Stock Reco Item", "_Test Warehouse Ledger 1 - _TC", 100],
 				[items["item_code"], items["warehouse"], items["qty"]])
 
-		else:	
+		else:
 			self.assertEqual(["_Test Stock Reco Item", "_Test Warehouse Ledger 1 - _TC", 100],
 				[items[0]["item_code"], items[0]["warehouse"], items[0]["qty"]])
 			
