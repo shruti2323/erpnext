b1f1fe0efef480d4ad1f360cde07c6e183bed8e4
mrunmay-epi <86222064+mrunmay-epi@users.noreply.github.com>
2021-08-26 19:12:06 +0530

    fix: BOM test cases (#1552)
    * chore: bom module test case fixed
    
    * chore:bom module test case updations
    
    Co-authored-by: mrunmay-epi <mrunmay.deshpande@extrapreneursindia.com>

--- erpnext/manufacturing/doctype/bom/test_bom.py
+++ erpnext/manufacturing/doctype/bom/test_bom.py
@@ -147,8 +147,9 @@ def reset_item_valuation_rate(item_code, warehouse_list=None, qty=None, rate=Non
 
 	if not warehouse_list:
 		warehouse_list = frappe.db.sql_list("""
-			select warehouse from `tabBin`
-			where item_code=%s and actual_qty > 0 and company = '_Test Company'
+			select tabBin.warehouse as warehouse from `tabBin` 
+			INNER JOIN tabWarehouse ON tabBin.warehouse=tabWarehouse.name 
+			where item_code = %s and actual_qty > 0 and company = '_Test Company';
 		""", item_code)
 
 		if not warehouse_list:
